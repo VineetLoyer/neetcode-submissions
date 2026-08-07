2f89986412b447298429728a3874585574e8170b
Vineet Kumar Loyer <vloyer@usc.edu>
2026-08-07 10:52:17 -0700

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int slow = 0;
+        int fast = 0;
+        int n = nums.length;
+        while(fast < n){
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+}
+
+// [3,2,2,3] val=3
+// slow=0,fast=0
+// fast<4 
+// nums[fast]=3, skip;fast=1
+// nums[fast]=2, = val -> swap. [2,3,2,3] slow=1, fast=2
+// nums[fast]=2, val -> swap. [2,2,3,3] slow=2, fast=3
+// fast<n -> exit return slow, 2
