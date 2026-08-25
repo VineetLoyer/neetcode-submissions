7601d03ed7a76ba27c4eace9376eb0f7ed18928e
Vineet Kumar Loyer <vloyer@usc.edu>
2026-08-25 07:33:25 -0700

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        if(n==0)return 0;
+        int len =1;
+        int maxLen=1;
+        for(int i=1; i<n;i++)
+        {
+            if(nums[i]==nums[i-1]){
+                continue;
+            }
+            if(nums[i]==nums[i-1]+1){
+                len++;
+            }
+            else{
+                len=1;
+            }
+            maxLen=Math.max(maxLen,len);
+        }
+        return maxLen;
+
+    }
+}
