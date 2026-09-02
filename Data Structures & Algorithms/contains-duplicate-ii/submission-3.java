ce7434c5d99c4c74fa263c4d30fab86e0cfed7f1
Vineet Kumar Loyer <vloyer@usc.edu>
2026-09-02 11:44:20 -0400

    Add: contains-duplicate-ii - submission-3

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int n = nums.length;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if (!hm.containsKey(nums[i])) {
+                hm.put(nums[i],i);
+            }
+            else{
+                int pastIndex = hm.get(nums[i]);
+                if (Math.abs(pastIndex - i)<= k){return true;}
+                else{hm.put(nums[i],i);}
+            }
+        }
+        return false;
+    }
+}
